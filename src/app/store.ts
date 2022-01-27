45867eab0a77e1d27066b008749f6df40f21bc49
Omid Ebrahimi <omid.ebrahimi.pro@gmail.com>
2022-01-27 14:40:35 +0330

    fixes non-serializable error in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import { configureStore } from '@reduxjs/toolkit';
+import { PersistConfig } from 'redux-persist/es/types';
+import { persistStore, persistReducer } from 'redux-persist'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist/es/constants';
 import stepsReducer from '../features/steps/stepsSlice';
-import { PersistConfig } from "redux-persist/es/types";
-import { StepsState } from "../features/steps/stepsTypes";
+import { StepsState } from '../features/steps/stepsTypes';
 
 const persistConfig: PersistConfig<StepsState> = {
   key: 'root',
@@ -13,7 +14,15 @@ const persistConfig: PersistConfig<StepsState> = {
 
 const persistedReducer = persistReducer(persistConfig, stepsReducer)
 
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      }
+    })
+});
 export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
