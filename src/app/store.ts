bdc30da6db249e444ff1e810ae6e9b1a83af5f0f
Omid Ebrahimi <omid.ebrahimi.pro@gmail.com>
2022-01-27 14:06:36 +0330

    persists progress in local storage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import stepsReducer from '../features/steps/stepsSlice';
+import { PersistConfig } from "redux-persist/es/types";
+import { StepsState } from "../features/steps/stepsTypes";
 
-export const store = configureStore({ reducer: stepsReducer });
+const persistConfig: PersistConfig<StepsState> = {
+  key: 'root',
+  storage,
+  blacklist: ["_persist"]
+}
+
+const persistedReducer = persistReducer(persistConfig, stepsReducer)
+
+export const store = configureStore({ reducer: persistedReducer });
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
